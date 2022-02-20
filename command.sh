5db132bdddc237bc93d4d43d23665577ffbf162e
liandongfu <liandongfu@gmail.com>
2022-02-20 14:33:26 +0900

    その他

--- command.sh
+++ command.sh
@@ -10,3 +10,9 @@ kubectl get pods <pod-name> -o yaml
 pods=$(kubectl get pods --selector=job-name=pi --output=jsonpath={.items[*].metadata.name})
 kubectl logs $pods
 
+
+kubectl expose deployment postgres --type=LoadBalancer --name=postgres-service
+
+# create a template
+kubectl create job hello-3 --from=cronjob/hello --dry-run=client -o yaml > hello.yaml
+kubectl create job hello-3 --from=cronjob/hello --dry-run -o yaml > hello.yaml
