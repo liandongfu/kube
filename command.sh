5ae37556d8e8132893b0181690d8fb521c313d37
liandongfu <liandongfu@gmail.com>
2022-02-24 18:36:23 +0900

    add spring boot app

--- command.sh
+++ command.sh
@@ -12,7 +12,20 @@ kubectl logs $pods
 
 
 kubectl expose deployment postgres --type=LoadBalancer --name=postgres-service
+kubectl expose deployment flask-cache  --type=LoadBalancer --port=8080
+kubectl expose deployment hello-minikube1  --type=LoadBalancer --port=8080
 
 # create a template
 kubectl create job hello-3 --from=cronjob/hello --dry-run=client -o yaml > hello.yaml
 kubectl create job hello-3 --from=cronjob/hello --dry-run -o yaml > hello.yaml
+
+kubectl create deployment hello-minikube1 --image=k8s.gcr.io/echoserver:1.4
+
+kubectl create deployment spring-boot-ap-dp --image spring-boot-ap:latest -o yaml --dry-run=client > spring-boot.yaml
+
+
+$ kubectl create service clusterip spring-boot-ap-dp --tcp 80:8080 -o yaml --dry-run=client > service/service.yaml
+
+eval $(minikube -p minikube docker-env)
+
+kubectl port-forward svc/spring-boot-ap-dp 9090:80
